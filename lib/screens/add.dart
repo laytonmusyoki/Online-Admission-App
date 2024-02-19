38bd4a5b23b4b222bfce8fccd84acb4ab12706f2
layton <laytonmatheka@gmail.com>
2024-02-19 13:33:34 +0300

    get api

--- lib/screens/add.dart
+++ lib/screens/add.dart
@@ -34,7 +34,7 @@ class _AddTodoState extends State<AddTodo> {
       };
       //   submit data to the server
       print(body);
-      final url='https://8407-41-90-70-190.ngrok-free.app/api/add';
+      final url='https://b30d-105-167-121-35.ngrok-free.app/api/add';
       final uri=Uri.parse(url);
       final response= await http.post(uri,
           body: jsonEncode(body),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -11,6 +12,14 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
+
+    Future <void> getStudents()async {
+      final url='https://b30d-105-167-121-35.ngrok-free.app/api/students';
+      final uri=Uri.parse(url);
+      final response= await http.get(uri);
+      print(response.body);
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -45,31 +54,36 @@ class _HomescreenState extends State<Homescreen> {
           children: [
             Row(
               children: [
-                Container(
-                  width: 100,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: Colors.blue.withOpacity(0.3),
-                    borderRadius: BorderRadius.circular(100),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 5.0,
-                            blurRadius: 7,
-                            offset: Offset(0,3)
+                GestureDetector(
+                  child: Container(
+                    width: 100,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(100),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5.0,
+                              blurRadius: 7,
+                              offset: Offset(0,3)
+                          ),
+                        ]
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Daily",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold
                         ),
-                      ]
-                  ),
-                  child: Center(
-                    child: Text(
-                      "Daily",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold
                       ),
                     ),
                   ),
+                  onTap: ()=>{
+                    getStudents()
+                  },
                 ),
                 SizedBox(width: 60,),
                 Container(
